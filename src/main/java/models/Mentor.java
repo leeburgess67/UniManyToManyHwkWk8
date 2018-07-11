417e69c9c22f34a5476166566553446ff52339cf
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-11 15:24:43 +0100

    mentor added

---
+++ src/main/java/models/Mentor.java
@@ -0,0 +1,52 @@
+package models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "mentors")
+public class Mentor {
+
+    private int id;
+    private String name;
+    private Student student;
+
+    public Mentor(){
+
+    }
+
+    public Mentor(String name, Student student){
+        this.name = name;
+        this.student = student;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
